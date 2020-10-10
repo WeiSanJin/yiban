3ceea49d818a4f6f79e0418b7b5bdc6864282a95
Wwx0410 <1162335221@qq.com>
2020-10-10 12:38:05 +0800

    :wrench:批量打卡账号信息

---
+++ userData.py
@@ -0,0 +1,13 @@
+data = [
+    {
+        'ACCOUNT': '13888888888',
+        'PASSWD': 'WeiSanJin',
+        'Email': '1162335221@qq.com'
+    },
+    {
+        'ACCOUNT': '13666666666',
+        'PASSWD': 'WeiSanJin',
+        'Email': '1162335221@qq.com'
+    }
+]
+
