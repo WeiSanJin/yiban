35909822e1c9c4f8f5f9f4d9b56d15c01d63e7fa
Wwx0410 <1162335221@qq.com>
2020-10-19 09:23:05 +0800

    :wrench:获取表单数据

---
+++ getInitiateId.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# @Time :2020/3/7 13:45
+# @File : getInitiateId.py
+# @Author : WeiSanJIn
+# @Software: PyCharm
+from utils import parse_data
+
+def getInitiateId(yb, type):
+    """
+        :param yb: 实例化yb对象
+        :param type:  返回数据类型0：最新表单 1：前一天表单
+    """
+    # 通过访问任务列表获取TaskId
+    taskList = yb.taskList()
+    TaskId = taskList.get('data')['list'][0]["LinkTo"].split('=')[1]
+    CompletedList = yb.getTaskDetail(TaskId)
+    # 我的任务
+    InitiateId = CompletedList.get('data')['InitiateId']
+    if TaskId == 'view&id' or InitiateId is not None:
+        for i in range(10):
+            print(i)
+            TaskId = taskList.get('data')['list'][i]["LinkTo"].split('=')[1]
+            if TaskId != 'view&id':
+                CompletedList = yb.getTaskDetail(TaskId)
+                InitiateId = CompletedList.get('data')['InitiateId']
+                break
+
+    shareUrl = 'https://app.uyiban.com/workflow/client/#/share?initiateId=' + InitiateId
+
+    # 防止链接失效,再次获取分享链接
+    ShareUrl = yb.getShareUrl(InitiateId)
+
+    # 将最新的分享链接写入 config.txt
+    with open('config.txt', 'w') as f:
+        # print('\n' + "分享链接：" + shareUrl)
+        f.write(shareUrl)
+
+    # 返回最新的表单数据
+    return parse_data(shareUrl, yb)
+
+
+def latestShareUrl(yb):
+    """
+        :param yb: 实例化yb对象
+    """
+    # 通过访问任务列表获取TaskId
+    taskList = yb.taskList()
+    TaskId = taskList.get('data')['list'][0]["LinkTo"].split('=')[1]
+    if TaskId == 'view&id':
+        for i in range(5):
+            TaskId = taskList.get('data')['list'][i]["LinkTo"].split('=')[1]
+            if TaskId != 'view&id':
+                break
+
+    CompletedList = yb.getTaskDetail(TaskId)
+    InitiateId = CompletedList.get('data')['InitiateId']
+    shareUrl = 'https://app.uyiban.com/workflow/client/#/share?initiateId=' + InitiateId
+
+    # 防止链接失效,再次获取分享链接
+    ShareUrl = yb.getShareUrl(InitiateId)
+
+    # 将最新的分享链接写入 config.txt
+    with open('config.txt', 'w') as f:
+        # print('\n' + "分享链接：" + str(ShareUrl['data']['uri']))
+        f.write(str(shareUrl))
+
+    # 返回最新的分享链接
+    print("激活分享链接：" + str(shareUrl))
+    return shareUrl
